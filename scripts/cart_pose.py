690e9acfd9312294e4736550623504af96bc673a
Hippolyte Watrelot <125671099+HippolyteWatrelot@users.noreply.github.com>
2023-05-24 10:49:08 +0200

    Update cart_pose.py

--- scripts/cart_pose.py
+++ scripts/cart_pose.py
@@ -32,25 +32,25 @@ def transmatrix_to_posestamped():
     p = PoseStamped()
     trace = R[0, 0] + R[1, 1] + R[2, 2]
     if trace > 0:
-        S = sqrt(1 + trace) * 2
+        S = np.sqrt(1 + trace) * 2
         qw = S / 4
         qx = (R[2, 1] - R[1, 2]) / S
         qy = (R[0, 2] - R[2, 0]) / S
         qz = (R[1, 0] - R[0, 1]) / S
     elif (R[0, 0] > R[1, 1]) and (R[0, 0] > R[2, 2]):
-        S = sqrt(1 + R[0, 0] - R[1, 1] - R[2, 2]) * 2
+        S = np.sqrt(1 + R[0, 0] - R[1, 1] - R[2, 2]) * 2
         qw = (R[2, 1] - R[1, 2]) / S
         qx = S / 4
         qy = (R[0, 1] + R[1, 0]) / S
         qz = (R[0, 2] + R[2, 0]) / S
     elif R[1, 1] > R[2, 2]:
-        S = sqrt(1 + R[1, 1] - R[0, 0] - R[2, 2]) * 2
+        S = np.sqrt(1 + R[1, 1] - R[0, 0] - R[2, 2]) * 2
         qw = (R[0, 2] - R[2, 0]) / S
         qx = (R[0, 1] + R[1, 0]) / S
         qy = S / 4
         qz = (R[1, 2] + R[2, 1]) / S
     else:
-        S = sqrt(1 + R[2, 2] - R[0, 0] - R[1, 1]) * 2
+        S = np.sqrt(1 + R[2, 2] - R[0, 0] - R[1, 1]) * 2
         qw = (R[1, 0] - R[0, 1]) / S
         qx = (R[0, 2] + R[2, 0]) / S
         qy = (R[1, 2] + R[2, 1]) / S
