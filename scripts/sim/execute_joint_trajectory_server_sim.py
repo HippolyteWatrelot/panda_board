9cb99c46452e3d3a48a088343b4a2f74aaded802
Hippolyte Watrelot <125671099+HippolyteWatrelot@users.noreply.github.com>
2023-06-14 00:59:38 +0200

    Update execute_joint_trajectory_server_sim.py

--- scripts/sim/execute_joint_trajectory_server_sim.py
+++ scripts/sim/execute_joint_trajectory_server_sim.py
@@ -1,3 +1,10 @@
 import rospy
 import numpy as np
 from panda_robot import PandaArm
+import os
+import time
+import sys
+
+from panda_board.srv import ExecuteJointTrajectory
+
+from sensor_msgs.msg import JointState
